2c81ef7d2f0c9c84a6a324f2b40ac8554b4313b9
fernandorussie <fernandocesarrussiefelip@gmail.com>
2022-12-14 09:42:07 -0300

    update5

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-//Consumindo a API para requisição
-
-fetch("http://localhost:3000/sanduiches")
+//Consumindo a API para requisição //SANDUICHES
+fetch("https://hamburgueriaapi.onrender.com/sanduiches")
 .then((resp) => resp.json())
-.then((data) => {exibirProdutos(data),console.log(data)})
+.then((data) => {qtSanduiche(data),console.log(data)})
 
+//Função qt produtos
+function qtSanduiche(quantidade) {
+    var qtSanduiche = quantidade.length
+    console.log("Sanduiches")
+    document.querySelector("#qtSanduiche").innerHTML = `x ${qtSanduiche}`
+}
+
+
+//Consumindo a API para requisição //Bebidas
+fetch("https://hamburgueriaapi.onrender.com/bebidas")
+.then((resp) => resp.json())
+.then((data) => {qtBebida(data),console.log(data)})
+
+//Função qt produtos
+function qtBebida(quantidade) {
+    var qtBebida = quantidade.length
+    console.log("Bebidas")
+    document.querySelector("#qtBebida").innerHTML = `x ${qtBebida}`
+}
+
+//Consumindo a API para requisição //Acompanhamentos
+fetch("https://hamburgueriaapi.onrender.com/acompanhamentos")
+.then((resp) => resp.json())
+.then((data) => {qtAcomp(data),console.log(data)})
+
+//Função qt produtos
+function qtAcomp(quantidade) {
+    var qtAcomp = quantidade.length
+    console.log("Acompanhamentos")
+    document.querySelector("#qtAcomp").innerHTML = `x ${qtAcomp}`
+}
+
+//Consumindo a API para requisição //Acompanhamentos
+fetch("https://hamburgueriaapi.onrender.com/categoria")
+.then((resp) => resp.json())
+.then((data) => {qtCategoria(data),console.log(data)})
+
+//Função qt produtos
+function qtCategoria(quantidade) {
+    var qtCategoria = quantidade.length
+    console.log("Categoria")
+    document.querySelector("#qtCategoria").innerHTML = `x ${qtCategoria}`
+}
 
 //Função para exibir os produtos
 
